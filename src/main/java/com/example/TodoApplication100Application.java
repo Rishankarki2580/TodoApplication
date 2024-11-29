05df3b0e872107b501e542dbf4c5515cf7426916
Rishan Karki <rishankarki2580@gmail.com>
2024-11-29 23:25:25 +0545

    first commit

---
+++ src/main/java/com/example/TodoApplication100Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication100Application.class, args);
+	}
+
+}
